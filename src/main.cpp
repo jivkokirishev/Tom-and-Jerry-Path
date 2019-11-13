a29d9601b189af33f34234e5b583031fece0ad88
jivkokirishev <jivkokirishev@gmail.com>
2019-11-13 17:57:14 +0200

    project configuration added

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by jivko on 13.11.19.
+//
+
+#ifndef TOM_AND_JERRY_PATH_MAIN_CPP
+#define TOM_AND_JERRY_PATH_MAIN_CPP
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "In progress.\n";
+
+    return 0;
+}
+
+
+#endif //TOM_AND_JERRY_PATH_MAIN_CPP
